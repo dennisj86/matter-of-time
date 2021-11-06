8c44b768ffd29ada5b9203f57cdd940150eb277a
Dennis <dennisj86@protonmail.com>
2021-11-06 16:19:56 +0100

    routes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :markets, dependent: :destroy
+  has_many :bookings, through: :market
+  has_many :messages, through: :booking
 end
