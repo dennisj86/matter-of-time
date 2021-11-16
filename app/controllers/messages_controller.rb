dffefbece29545cb901a9a9c2b58b7f0ef5a7e9b
Martin Krause <findmemart@gmail.com>
2021-11-16 22:58:12 +0100

    working on show->message->booking

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,4 +7,34 @@ class MessagesController < ApplicationController
     skip_policy_scope
   end
 
+  def new
+    @booking = Booking.new(booking_params)
+    @booking = Booking.find(params[:booking_id])
+    @market = Market.find(params[:market_id])
+    @message = Message.new
+    authorize @message
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking = Booking.find(params[:booking_id])
+    @market = Market.find(params[:market_id])
+    @message = Message.new(message_params)
+    @booking.market = @market
+    @booking.message = @message
+    @booking.user = @user
+    @message.user = @user
+    @booking.save
+    @message.save
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :status)
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :markets do
-    resources :bookings
+    resources :bookings do
+      resources :messages
+    end
   end
   resources :dashboard, only: %i[index]
   resources :messages, only: %i[index create]
