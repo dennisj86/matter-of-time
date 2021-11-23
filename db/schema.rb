ac9db269d79f431c9f918588c6906b848ef9953c
Natalie Bechtold <natalie.bechtold@gmail.com>
2021-11-23 21:15:41 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_105702) do
+ActiveRecord::Schema.define(version: 2021_11_23_115702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(version: 2021_11_23_105702) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.integer "time"
+    t.string "location"
+    t.integer "remote"
     t.index ["user_id"], name: "index_markets_on_user_id"
   end
 
